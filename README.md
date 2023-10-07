da7f1181427603d816a38125d8e99134cab7232b
Philippe Serhal <philippe.serhal@gmail.com>
2023-10-07 17:30:16 -0400

    feat: add basic professional Gatsby website

---
+++ README.md
@@ -0,0 +1,5 @@
+# philippeserhal.com
+
+My professional portfolio website, located at www.philippeserhal.com.
+
+Made with [Gatsby](https://www.gatsbyjs.com).
---
+++ gatsby-config.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Philippe Serhal - software developer`,
+    siteUrl: `https://www.philippeserhal.com`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [process.env.GOOGLE_ANALYTICS_TRACKING_ID],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+  ],
+};
+
+export default config;
