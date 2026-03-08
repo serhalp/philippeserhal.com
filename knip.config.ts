f3bf9441ff91264ad34bc275a098a1a7d5fa24e6
Philippe Serhal <philippe.serhal@gmail.com>
2026-03-08 17:52:53 -0400

    ci: add missing knip workflow (#138)

--- knip.config.ts
+++ knip.config.ts
@@ -1,5 +1,5 @@
 import type { KnipConfig } from "knip";
 
 export default {
-  ignoreDependencies: ["eslint-plugin-jsx-a11y"],
+  ignoreDependencies: ["eslint-plugin-jsx-a11y", "@unocss/reset"],
 } satisfies KnipConfig;
